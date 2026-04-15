6bcff3e51708b852481a452dbd90fb8430236deb
Shivang Kheradiya <31017420+shivangKheradiya@users.noreply.github.com>
2026-04-15 08:35:07 +0000

    database.py added

--- OPE_DB_API/__init__.py
+++ OPE_DB_API/__init__.py
@@ -7,8 +7,8 @@ Session-based JSONB editing and query engine
 from .metadata import __version__
 
 from .main import create_app
-from .api.jsonb_api import router as jsonb_router
-from .api.session_api import router as session_router
+# from .api.jsonb_api import router as jsonb_router
+# from .api.session_api import router as session_router
 
 __all__ = [
     "create_app",
--- OPE_DB_API/metadata.py
+++ OPE_DB_API/metadata.py
@@ -1,3 +1,3 @@
 __version__ = "0.1.0"
 
-__api_title__ = "Dynamic OPE_DB_API CRUD API"
+__api_title__ = "OPE_DB_API : Dynamic Core backend API service for Open Plant Engineering."
