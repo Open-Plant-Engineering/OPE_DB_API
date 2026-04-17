45c02fd314129258de39bef1b10620b47f02aa7a
Shivang Kheradiya <31017420+shivangKheradiya@users.noreply.github.com>
2026-04-17 10:26:33 +0000

    WorkPushRequest will store all values

--- OPE_DB_API/schemas/work.py
+++ OPE_DB_API/schemas/work.py
@@ -1,12 +1,12 @@
 from pydantic import BaseModel
-from typing import Optional, Dict, List
+from typing import Optional, List, Any
 
 class WorkPushRequest(BaseModel):
     data_id: Optional[int]     # None = CREATE
     node_id: int
     attribute_id: int
     operation_type: int        # 1 = CREATE, 2 = UPDATE, 3 = DELETE
-    value: Optional[Dict]      # None only allowed for DELETE
+    value: Optional[Any]      # None only allowed for DELETE
 
 class BulkWorkPushRequest(BaseModel):
     items: List[WorkPushRequest]
