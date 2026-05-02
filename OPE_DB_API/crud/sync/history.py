6ab933b1047d136b3934c8f48bd2777c21bc5ace
Shivang Kheradiya <shivangatul@gmail.com>
2026-05-02 20:09:52 +0200

    History Response updated for Replying history

--- OPE_DB_API/crud/sync/history.py
+++ OPE_DB_API/crud/sync/history.py
@@ -3,7 +3,7 @@ from sqlalchemy import and_
 from datetime import datetime
 from typing import List
 
-from OPE_DB_API.registry import HISTORY_TABLE_REGISTRY
+from OPE_DB_API.registry import HISTORY_TABLE_REGISTRY, LIVE_TABLE_REGISTRY
 from OPE_DB_API.models.session import SessionMetadata
 
 
@@ -18,9 +18,20 @@ def fetch_history_after(
     """
 
     history_model = HISTORY_TABLE_REGISTRY[domain]
+    live_model = LIVE_TABLE_REGISTRY[domain]
 
-    return (
-        db.query(history_model)
+    rows = (
+        db.query(
+            history_model.data_id,
+            history_model.operation_type,
+            history_model.new_value,
+            live_model.attribute_id,
+            live_model.node_id,
+        )
+        .outerjoin(
+            live_model,
+            live_model.data_id == history_model.data_id,
+        )
         .join(
             SessionMetadata,
             SessionMetadata.session_id == history_model.session_id,
@@ -38,3 +49,14 @@ def fetch_history_after(
         .limit(limit)
         .all()
     )
+
+    return [
+        {
+            "data_id": r.data_id,
+            "node_id": r.node_id,
+            "attribute_id": r.attribute_id,
+            "operation_type": r.operation_type,
+            "new_value": r.new_value,
+        }
+        for r in rows
+    ]
